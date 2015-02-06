d299d268802d6b2f81923890a8dbd5c2cf0f38d8
Nathan Kern <natek5309@gmail.com>
2015-02-05 17:23:22 -0800

    Clean up unused lines

--- stica_test.py
+++ stica_test.py
@@ -2,22 +2,17 @@
 
 import sima
 import sima.segment
-#import sima.motion
-#from sima.iterables import MultiPageTIFF
 
 video = "image.tif"
 analysis_dir = "stica_test.sima"
 
 print "[Creating SIMA analysis directory: %s]" % analysis_dir
-#iterables = [[MultiPageTIFF(video)]]
-#dataset = sima.ImagingDataset(iterables, analysis_dir)
 seq = sima.Sequence.create('TIFF', 'original.tif')
 dataset = sima.ImagingDataset([seq], analysis_dir)
 
 print "[Finding ROIs using stICA method (this could take a while . . .)]"
 stica = sima.segment.STICA(components=5)
 rois = dataset.segment(stica, label="stICA ROIs")
-import pdb; pdb.set_trace()
 print dataset.ROIs.keys()
 
 print "[Done]"
