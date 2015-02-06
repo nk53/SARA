60b67f9d8f7e8a72a672aadc1e38f075a6d5b8a5
Nathan Kern <natek5309@gmail.com>
2015-02-05 17:15:57 -0800

    Update for sima 1.0.x (devel)

--- hmm_test.py
+++ hmm_test.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 
 import sima
-import sima.motion
-from sima.iterables import MultiPageTIFF
+from sima.motion import HiddenMarkov2D
 
 video = "image.tif"
 analysis_dir = "hmm_test.sima"
 
-print "[Creating SIMA analysis directory: %s]" % analysis_dir
-iterables = [[MultiPageTIFF(video)]]
-
-print "[Applying hidden Markov model]"
-dataset = sima.motion.hmm(iterables, analysis_dir, channel_names=['hmm_test'], max_displacement=[10, 10])
+seq = sima.Sequence.create('TIFF', 'original.tif')
+ds = HiddenMarkov2D(max_displacement=[50, 50], n_processes=2).correct([seq], 'test-sima-1.0.sima')
+ds.export_frames([[['corrected.tif']]])
