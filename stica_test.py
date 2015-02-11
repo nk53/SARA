6a8c7551716d2c90a97224368f5f61348f14477a
Nathan Kern <natek5309@gmail.com>
2015-02-11 15:58:16 -0800

    Use consistent naming

--- stica_test.py
+++ stica_test.py
@@ -3,11 +3,11 @@
 import sima
 import sima.segment
 
-video = "image.tif"
+video = "original.tif"
 analysis_dir = "stica_test.sima"
 
 print "[Creating SIMA analysis directory: %s]" % analysis_dir
-seq = sima.Sequence.create('TIFF', 'original.tif')
+seq = sima.Sequence.create('TIFF', video)
 dataset = sima.ImagingDataset([seq], analysis_dir)
 
 print "[Finding ROIs using stICA method (this could take a while . . .)]"
