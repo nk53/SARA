044a71a65f9a73c9054e9676eb1db1cf04b1b0fe
Nathan Kern <natek5309@gmail.com>
2015-02-11 16:13:25 -0800

    Test different overlap params

--- stica_20ol.py
+++ stica_20ol.py
@@ -4,14 +4,14 @@ import sima
 import sima.segment
 
 video = "original.tif"
-analysis_dir = "stica_0ol.sima"
+analysis_dir = "stica_20ol.sima"
 
 print "[Creating SIMA analysis directory: %s]" % analysis_dir
 seq = sima.Sequence.create('TIFF', video)
 dataset = sima.ImagingDataset([seq], analysis_dir)
 
 print "[Finding ROIs using stICA method (this could take a while . . .)]"
-stica = sima.segment.STICA(components=5)
+stica = sima.segment.STICA(components=5, overlap_per=0.2)
 rois = dataset.segment(stica, label="stICA ROIs")
 print dataset.ROIs.keys()
 
