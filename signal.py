47397a2419dea1e583e8b992266e61915cd39a22
Nathan Kern <natek5309@gmail.com>
2015-02-22 16:24:25 -0800

    Remove debugging line

--- signal.py
+++ signal.py
@@ -9,7 +9,6 @@ print "[Loading SIMA analysis directory: %s]" % analysis_dir
 dataset = ImagingDataset.load(analysis_dir)
 
 signal = dataset.signals()['signal']
-import pdb; pdb.set_trace()
 dataset.save(analysis_dir)
 dataset.export_signals('all.csv')
 
