ab55bf69514c0668adbe6357a8c8bf989a180202
Nathan Kern <natek5309@gmail.com>
2015-02-25 12:33:37 -0800

    Make procedure more general

--- signal.py
+++ signal.py
@@ -4,11 +4,12 @@ from sima.imaging import ImagingDataset
 
 video = "image.tif"
 analysis_dir = "stica_test.sima"
+outfile = "all.csv"
 
 print "[Loading SIMA analysis directory: %s]" % analysis_dir
 dataset = ImagingDataset.load(analysis_dir)
 
 signal = dataset.signals()['signal']
-dataset.export_signals('all.csv')
+dataset.export_signals(outfile)
 
 print "[Done]"
