54d702a338661b418047de51c39c7c63405b3411
Nathan Kern <natek5309@gmail.com>
2015-02-27 15:19:10 -0800

    plot multiple parameters' output

--- visualize.py
+++ visualize.py
@@ -4,8 +4,10 @@ from sima.ROI import ROIList
 from PIL import Image
 import matplotlib.pyplot as plt
 
-# SIMA anlaysis directory
-analysis_dir = "stica_test.sima"
+# SIMA anlaysis directories
+dirs = ['stica_0ol.sima', 'stica_20ol.sima', 'stica_50ol.sima',
+        'stica_5mu.sima', 'stica_9mu.sima', 'stica_50pca.sima',
+        'stica_75pca.sima']
 
 # change default line colors and line width
 #plt.rc('axes', color_cycle = ['b', 'r', 'm', 'brown', 'k', 'grey'])
@@ -14,23 +16,25 @@ cc = ['blue', 'red', 'magenta', 'brown', 'cyan', 'orange',
 plt.rc('axes', color_cycle = cc) 
 plt.rc('lines', linewidth=2)
 
-# prepare background image
-im = Image.open("rgb.png")
-im_width, im_height = im.size
-plt.xlim(xmin=0, xmax=im_width)
-plt.ylim(ymin=0, ymax=im_height)
-plt.imshow(im)
-
-# get list of ROIs from SIMA analysis directory
-rois = ROIList.load(path_join(analysis_dir, "rois.pkl"))
-
-# plot all of the ROIs, warn if an ROI has more than one coordinate set
-for roi in rois:
-  coords = roi.coords
-  if len(coords) > 1:
-    print "Warning: Roi%s has >1 coordinate set" % roi.id
-  x = coords[0][:,0]
-  y = coords[0][:,1]
-  plt.plot(x, y)
-
-plt.show()
+for analysis_dir in dirs:
+  # prepare background image
+  im = Image.open("rgb.png")
+  im_width, im_height = im.size
+  plt.xlim(xmin=0, xmax=im_width)
+  plt.ylim(ymin=0, ymax=im_height)
+  plt.imshow(im)
+  
+  # get list of ROIs from SIMA analysis directory
+  rois = ROIList.load(path_join(analysis_dir, "rois.pkl"))
+  
+  # plot all of the ROIs, warn if an ROI has more than one coordinate set
+  for roi in rois:
+    coords = roi.coords
+    if len(coords) > 1:
+      print "Warning: Roi%s has >1 coordinate set" % roi.id
+    x = coords[0][:,0]
+    y = coords[0][:,1]
+    plt.plot(x, y)
+  
+  plt.savefig(analysis_dir + '.tif')
+  plt.clf()
