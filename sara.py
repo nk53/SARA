bee2f6beae1f0b7adb1c515ea1696ed4bf2c34e1
Nathan Kern <natek5309@gmail.com>
2015-02-27 17:32:24 -0800

    More PlaneTranslation2D motion correction changes
    Should work now, although SIMA is giving me a
    MaximumRecursion depth exceeded error or something

--- sara.py
+++ sara.py
@@ -1,4 +1,5 @@
-from os import stat
+from os.path import isfile, isdir
+from sys import stdout
 from IPython.html import widgets
 from IPython.display import display
 from sima import Sequence
@@ -19,14 +20,33 @@ class SaraUI():
     if prompt == None:
       prompt = "Please input the full path to the file: "
     path = raw_input(prompt)
-    try:
-      stat(path)
-    except OSError as e:
-      if e.errno == 2:
-        prompt = "The file path you input is invalid, please try again: "
-        return self.getFilePath(prompt)
+    if not isfile(path):
+      prompt = "The file path you input is invalid, please try again: "
+      return self.getFilePath(prompt)
     return path
   
+  def reserveFilePath(self, prompt=None, allow_overwrite=False):
+    if prompt == None:
+      prompt = "Please input file name: "
+    path = raw_input(prompt)
+    if not allow_overwrite and isfile(path):
+      prompt = "The name you entered already exists, please try again: "
+      return self.reserveFilePath(prompt)
+    if isdir(path):
+      prompt = "The name you entered is a directory, please try again: "
+      return self.reserveFilePath(prompt)
+    return path
+  
+  def reserveDirectory(self, prompt=None):
+    if prompt == None:
+      prompt = "Please input a directory name: "
+    path = raw_input(prompt)
+    if isdir(path):
+      prompt = "The directory you entered already exists, " + \
+                "please try again: "
+      return self.reserveDirectory(prompt)
+    return path
+    
   def getInteger(self, prompt=None):
     if prompt == None:
       prompt = "Please enter an integer: "
@@ -37,6 +57,7 @@ class SaraUI():
       prompt = "The value you entered is not a valid integer, " + \
                 "please try again: "
       self.getInteger(prompt)
+    return integer
   
   def getNatural(self, prompt=None):
     if prompt == None:
@@ -93,6 +114,8 @@ class MotionCorrectionUI(SaraUI):
   def correct(self, file_path=None, verbose=True):
     if self.strategy_radio == None:
       print "You need to select a motion correction strategy first"
+      return
+    self.strategy_radio.close()
     # call the correction function
     self.strategy_map[self.strategy_radio.value](file_path, verbose)
 
@@ -104,14 +127,20 @@ class MotionCorrectionUI(SaraUI):
 
     self.sequence = Sequence.create('TIFF', image_path)
     prompt = "Name of SIMA analysis directory: "
-    project_directory = self.getString(prompt)
+    project_directory = self.reserveFilePath(prompt)
     prompt = ["Maximum %s displacement (in pixels): " \
                % ax for ax in ['X', 'Y']]
     md_x = self.getNatural(prompt[0])
     md_y = self.getNatural(prompt[1])
+    prompt = "Where would you like to save the corrected frames? "
+    corrected_frames = self.reserveDirectory(prompt)
     settings = {'max_displacement':[md_x, md_y]}
+    print "Performing motion correction (this could take a while) . . ."
+    stdout.flush() # force print statement to output to IPython
     self.dataset = PlaneTranslation2D(**settings).correct(
                      [self.sequence], project_directory)
+    print "Motion correction complete"
+    self.dataset.export_frames([[[corrected_frames]]])
   
   def hmm(self):
     pass
