87ae7f62c6600b085a3376a118d92be625912aad
Nathan Kern <natek5309@gmail.com>
2015-03-04 14:42:12 -0800

    Many, many updates
    Added id-post processing, defined getBoolean(), made reserveFilePath()
    ask if user wants to overwrite file, updated other prompts and added
    segmentation (currently incomplete)

--- sara.py
+++ sara.py
@@ -4,18 +4,29 @@ from IPython.html import widgets
 from IPython.display import display
 from sima import Sequence
 from sima.motion import PlaneTranslation2D
+from sima.ROI import ROI, ROIList
+from sima.segment import STICA
+from sima.segment.segment import PostProcessingStep
 
 class SaraUI():
-  def __init__(self):
-    self.dataset = None
+  def __init__(self, sima_dir=None):
+    if sima_dir == None:
+     prompt = "Name of SIMA analysis directory: "                   
+     project_directory = self.reserveDirectory(prompt, '.sima')
   
   def getBoolean(self, prompt):
-    pass
+    """Expects a response of the form (y/n)"""
+    bool_map = {'y' : True, 'n' : False}
+    response = raw_input(prompt)[0].lower()
+    if response != 'y' and response != 'n':
+      prompt = "Please enter y (yes) or n (no): "
+      return self.getBoolean(prompt)
+    return bool_map[response]
   
   def getFilePath(self, prompt=None):
     """Prompt the user for full path to a file
        
-       Also works if relative path is given"""
+       Also works if relative path is given; MUST NOT BE A DIRECTORY!"""
     # default prompt
     if prompt == None:
       prompt = "Please input the full path to the file: "
@@ -30,23 +41,42 @@ class SaraUI():
       prompt = "Please input file name: "
     path = raw_input(prompt)
     if not allow_overwrite and isfile(path):
-      prompt = "The name you entered already exists, please try again: "
-      return self.reserveFilePath(prompt)
+      prompt = "The name you entered already exists, do you want to " + \
+               "overwrite it? (y/n): "
+      if not self.getBoolean(prompt):
+        return self.reserveFilePath()
     if isdir(path):
       prompt = "The name you entered is a directory, please try again: "
       return self.reserveFilePath(prompt)
     return path
   
-  def reserveDirectory(self, prompt=None):
+  def reserveDirectory(self, prompt=None, extension=''):
     if prompt == None:
       prompt = "Please input a directory name: "
-    path = raw_input(prompt)
+    path = raw_input(prompt) + extension
+    if not path.endswith(extension):
+      path += extension
+    if isfile(path):
+      prompt = "The name you entered is a file and already exists, " + \
+               "please try again: "
     if isdir(path):
       prompt = "The directory you entered already exists, " + \
-                "please try again: "
-      return self.reserveDirectory(prompt)
+                "do you want to use it anyway? (y/n): "
+      if not self.getBoolean(prompt):
+        return self.reserveDirectory()
     return path
-    
+  
+  def getFloat(self, prompt=None):
+    if prompt == None:
+      prompt = "Please enter a decimal number (e.g. 1.51): "
+    try:
+      num = float(raw_input(prompt))
+    except ValueError:
+      prompt = "The value you entered is not a valid number, " + \
+               "please try again: "
+      num = self.getFloat(prompt)
+    return num
+  
   def getInteger(self, prompt=None):
     if prompt == None:
       prompt = "Please enter an integer: "
@@ -56,7 +86,7 @@ class SaraUI():
     except ValueError:
       prompt = "The value you entered is not a valid integer, " + \
                 "please try again: "
-      self.getInteger(prompt)
+      integer = self.getInteger(prompt)
     return integer
   
   def getNatural(self, prompt=None):
@@ -67,6 +97,22 @@ class SaraUI():
       prompt = "The number you entered is negative, please try again: "
       natural = self.getInteger(prompt)
     return natural
+  
+  def getPercent(self, prompt=None):
+    """Prompt user for something like 90 or 42.2%"""
+    if prompt == None:
+      "Please enter a percentage (e.g. 90.5%): "
+    # get rid of all '%' that appear
+    percent = -1
+    while percent < 0 or percent > 100:
+      try:
+        percent = float(raw_input(prompt).replace('%', ''))
+      except ValueError:
+        prompt = "The value you entered is not a valid percentage, " + \
+                 "please try again: "
+        continue
+      prompt = "Please enter a number between 0 and 100: "
+    return percent
   
   def getString(self, prompt=None):
     """Prompt the user for a string"""
@@ -101,6 +147,7 @@ class MotionCorrectionUI(SaraUI):
     self.strategy_map = {
       "2D Plane Correction" : self.planeTranslation2D,
       "Hidden Markov Model" : self.hmm,
+      "None"                : lambda x, y: None,
     }
     
     if showNow == True:
@@ -127,13 +174,13 @@ class MotionCorrectionUI(SaraUI):
 
     self.sequence = Sequence.create('TIFF', image_path)
     prompt = "Name of SIMA analysis directory: "
-    project_directory = self.reserveFilePath(prompt)
+    project_directory = self.reserveDirectory(prompt, '.sima')
     prompt = ["Maximum %s displacement (in pixels): " \
                % ax for ax in ['X', 'Y']]
     md_x = self.getNatural(prompt[0])
     md_y = self.getNatural(prompt[1])
     prompt = "Where would you like to save the corrected frames? "
-    corrected_frames = self.reserveDirectory(prompt)
+    corrected_frames = self.reserveFilePath(prompt)
     settings = {'max_displacement':[md_x, md_y]}
     print "Performing motion correction (this could take a while) . . ."
     stdout.flush() # force print statement to output to IPython
@@ -144,3 +191,56 @@ class MotionCorrectionUI(SaraUI):
   
   def hmm(self):
     pass
+
+class SegmentationUI(SaraUI):
+  def __init__(self):
+    # check if motion is defined
+    if 'motion' in dir():
+      self.dataset = motion.dataset
+    else:
+      self.dataset = None
+    self.mu = -1
+    self.components = -1
+    self.overlap_per = 0
+    self.rois = None
+  
+  def segment(self, settings=None):
+    if settings == None:
+      if self.dataset == None:
+        prompt = "Name of SIMA analysis directory: "
+        self.reserveDirectory(prompt)
+      else:
+        print "Using", self.dataset
+      prompt = "Number of PCA components (higher numbers take longer; " + \
+               "default 50): "
+      self.components = self.getNatural(prompt)
+      prompt = "Tradeoff between spatial and temporal information " + \
+               "(must be between 0 and 1; low values give higher " + \
+               "weight to temporal information; default 0.5): "
+      mu = -1
+      while self.mu < 0 or self.mu > 1:
+        self.mu = self.getFloat(prompt)
+      prompt = "Percent of ROIs that must overlap to be combined " + \
+               "(must be between 0 and 100; enter 0 to skip " + \
+               "this step; default 20): "
+      self.overlap_per = self.getPercent(prompt)
+      settings = {
+        'components' : self.components,
+        'mu' : self.mu,
+        'overlap_per' : self.overlap_per,
+      }
+      print "Performing Spatiotemporal Independent Component Analysis..."
+      stdout.flush()
+      stica = STICA(**settings)
+      stica.append(IdROIs())
+      self.rois = self.dataset.segment(stica, label="stICA ROIs")
+      print len(self.dataset.ROIs['stICA ROIs']), "ROIs found"
+
+class IdROIs(PostProcessingStep):
+  def apply(self, rois, dataset=None):
+    rois_with_ids = []
+    for index, roi in enumerate(rois):
+      newroi = roi.todict()
+      newroi['id'] = index
+      rois_with_ids.append(newroi)
+    return ROIList(rois_with_ids)
